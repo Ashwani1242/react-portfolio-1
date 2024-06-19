ef4f047001a9df010c5681b76046d5cb2f7c8fdc
Ashwani1242 <ashwanigupta1242@gmail.com>
2024-06-19 23:54:06 +0530

    Changes Committed on 2024-06-19, 23:54

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -60,15 +60,15 @@ const Navbar = (props) => {
           <p className='relative inset-0 flex items-center justify-center z-20 group-hover:text-white duration-500'> Contact </p>
           <div className='absolute inset-0 bg-black h-full w-0 flex items-center justify-center group-hover:w-full z-10 duration-500'> </div>
         </li>
-        <a href={resume} download='resume' className='bg-white cursor-pointer group relative py-2 px-4'>
-          <p className='relative inset-0 flex items-center justify-center z-20 group-hover:text-white duration-500'>
+        <li className='bg-white cursor-pointer group relative py-2 px-4'>
+          <a href={resume} download='resume' className='relative inset-0 flex items-center justify-center z-20 group-hover:text-white duration-500'>
             Resume
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={2} stroke="currentColor" className="size-6 pl-2">
               <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
             </svg>
-          </p>
+          </a>
           <div className='absolute inset-0 bg-black h-full w-0 flex items-center justify-center group-hover:w-full z-10 duration-500'> </div>
-        </a>
+        </li>
       </ul>
     </motion.div>
   )
